ba0e8715d7da3a168c7f721b3cc74a068ce3b46f
AAlexIII <a.alex.2000@mail.ru>
2019-09-13 04:12:28 +0300

    №5

---
+++ Пятница 13/call_tel.py
@@ -0,0 +1,12 @@
+class InvailidKod(Exception):
+    pass
+
+
+cost_for_call = {'343': 15, '381':18, '473': 13, '485':11}
+kod = input("Код города: ")
+if kod not in cost_for_call.keys():
+    raise InvailidKod(kod + "  номера нет в списке")
+time = float(input("Введите время разговора (мин.сек): "))
+print("Стоимость: ", cost_for_call[kod] * (int(time) + (time % 1)/0.6))
+
+
