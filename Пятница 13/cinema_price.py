596a4217c17a34399d73416c202b1b2a60d6351a
AAlexIII <a.alex.2000@mail.ru>
2019-09-13 04:34:15 +0300

    №5

---
+++ Пятница 13/cinema_price.py
@@ -0,0 +1,10 @@
+film_list = {"Пятница":[12,16,20],"Чемпионы":[10,13,16],"Пернатая банда": [10,14,18]}
+film = input('Выбрать фильм: ')
+day = input('Выбрать день (сегодня, завтра): ')
+time = input('Выбрать время: ')
+tiket_number = input('Выбрать количество билетов: ')
+print('Выбрали фильм: ' + film + ' День: ' + day +' Время: ' + time + 'Количество билетов: '+ tiket_number)
+if (film not in film_list.keys()) or (day not in ['завтра', 'сегодня']) or (time not in film_list[film]) or tiket_number < 1:
+    print('Ошибка ввода.')
+else:
+    if film == 'завтра':
