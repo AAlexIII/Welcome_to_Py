9283308e7a08aab74680d0977df1ae86d44db9fd
AAlexIII <55252571+AAlexIII@users.noreply.github.com>
2019-09-13 14:09:14 +0300

    Update cinema_price.py

--- Пятница 13/cinema_price.py
+++ Пятница 13/cinema_price.py
@@ -1,12 +1,17 @@
 # Не готово ещё
 
-film_list = {"Пятница":[12,16,20],"Чемпионы":[10,13,16],"Пернатая банда": [10,14,18]}
+film_list = {"Пятница":[{12:250},{16:350},{20:450}],"Чемпионы":[{10:250},{13:350},{16:350}],"Пернатая банда": [{10:350},
+{14:450},{18:450}]}
 film = input('Выбрать фильм: ')
 day = input('Выбрать день (сегодня, завтра): ')
-time = input('Выбрать время: ')
-tiket_number = input('Выбрать количество билетов: ')
-print('Выбрали фильм: ' + film + ' День: ' + day +' Время: ' + time + 'Количество билетов: '+ tiket_number)
-if (film not in film_list.keys()) or (day not in ['завтра', 'сегодня']) or (time not in film_list[film]) or tiket_number < 1:
+time = int(input('Выбрать время: '))
+tiket_number =int( input('Выбрать количество билетов: '))
+print('Выбрали фильм: ' + film + ' День: ' + day +' Время: ' + str(time) + 'Количество билетов: '+ str(tiket_number))
+if (film not in film_list.keys()) or (day not in ['завтра', 'сегодня']) or (time not in film_list[film].keys()) or tiket_number < 1:
     print('Ошибка ввода.')
 else:
-    if film == 'завтра':
+    if tiket_number >= 20:
+        tiket_number = tiket_number * 0.8
+    elif film == 'завтра':
+        tiket_number = tiket_number * 0.95
+    print("К оплате: " + film_list[film][time]*tiket_number)
