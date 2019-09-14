5d82df1ad7583748ff5a6ba64c159b5cc4e99306
AAlexIII <a.alex.2000@mail.ru>
2019-09-14 20:28:23 +0300

    последнее

--- Пятница 13/cinema_price.py
+++ Пятница 13/cinema_price.py
@@ -1,17 +1,21 @@
-# Не готово ещё
+def find_in_list(list, elem):
+    for i in list:
+        return i[elem] if elem in i.keys() else ''
 
-film_list = {"Пятница":[{12:250},{16:350},{20:450}],"Чемпионы":[{10:250},{13:350},{16:350}],"Пернатая банда": [{10:350},
-{14:450},{18:450}]}
-film = input('Выбрать фильм: ')
-day = input('Выбрать день (сегодня, завтра): ')
-time = int(input('Выбрать время: '))
-tiket_number =int( input('Выбрать количество билетов: '))
-print('Выбрали фильм: ' + film + ' День: ' + day +' Время: ' + str(time) + 'Количество билетов: '+ str(tiket_number))
-if (film not in film_list.keys()) or (day not in ['завтра', 'сегодня']) or (time not in film_list[film].keys()) or tiket_number < 1:
-    print('Ошибка ввода.')
+
+film_list = {"Пятница": [{12: 250}, {16: 350}, {20: 450}], "Чемпионы": [{10: 250}, {13: 350}, {16: 350}],
+             "Пернатая банда": [{10: 350}, {14: 450}, {18: 450}]}
+film = input("Выбрать фильм: ")
+day = input("Выбрать день (сегодня, завтра): ")
+time = int(input("Выбрать время: "))
+ticket_number = int(input("Выбрать количество билетов: "))
+print(f"Выбрали фильм: {film} День: {day} Время: {str(time)} Количество билетов: {str(ticket_number)}")
+if (film not in film_list.keys()) or (day not in ['завтра', 'сегодня']) or (find_in_list(film_list[film], time) == '') \
+        or (ticket_number < 1):
+    print("Ошибка ввода.")
 else:
-    if tiket_number >= 20:
-        tiket_number = tiket_number * 0.8
-    elif film == 'завтра':
-        tiket_number = tiket_number * 0.95
-    print("К оплате: " + film_list[film][time]*tiket_number)
+    if ticket_number >= 20:
+        ticket_number = ticket_number * 0.8
+    elif day == 'завтра':
+        ticket_number = ticket_number * 0.95
+    print(f"К оплате: {str(int(find_in_list(film_list[film], time) * ticket_number))}")
