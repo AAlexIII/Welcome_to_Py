0b06968921aa742d829b548dbdc38441deacc9a9
AAlexIII <a.alex.2000@mail.ru>
2019-09-19 11:45:05 +0300

    Исправлен код города на телефон в call_tell.py

--- 3 что-то 19.09/if_random_game.py
+++ 3 что-то 19.09/if_random_game.py
@@ -1,3 +1,20 @@
-print('sfnskrfnsrkf','fvkfdmvlkdvm')
-print('dfnkdjndbvd')
-print('kfdjbdjhfbvdjrhbvdrbdpkirnjdlk')
+import random
+
+
+def win_or_lose(a, b):
+    """
+    :param a: ваше число
+    :param b: рандом
+    :return: то что надо =)
+    """
+    if a == b:
+        return "Win"
+    else:
+        if a > b:
+            return "Слишком много"
+        else:
+            return "Слишком мало"
+
+
+print(win_or_lose(int(input("Введите число ")), random.randint(1, 4)))
+
